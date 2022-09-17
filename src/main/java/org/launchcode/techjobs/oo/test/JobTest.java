e9c4d4e7e9132d7c897229252c6a4a3fa3983aca
Rich Ives <rjsives89@gmail.com>
2022-09-17 00:41:39 -0500

    tests for equality and constructors added to JobTest class. Task 4 complete.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,41 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+@Test
+    public void testJobConstructorSetsAllFields(){
+    Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    assertEquals("Product tester", testJob.getName());
+    assertEquals("ACME", testJob.getEmployer().getValue());
+    assertEquals("Desert", testJob.getLocation().getValue());
+    assertEquals("Quality control", testJob.getPositionType().getValue());
+    assertEquals("Persistence", testJob.getCoreCompetency().getValue());
+
+    assertTrue(testJob instanceof Job);
+    assertTrue(testJob.getEmployer() instanceof Employer);
+    assertTrue(testJob.getLocation() instanceof Location);
+    assertTrue(testJob.getPositionType() instanceof PositionType);
+    assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+
+}
+
+@Test
+    public void testJobsForEquality(){
+    Job firstJob = new Job("Quality Assurance Manager", new Employer("Platinum Driveline"), new Location("Eureka"), new PositionType("Quality control"), new CoreCompetency("Ethical"));
+    Job secondJob = new Job("Quality Assurance Manager", new Employer("Platinum Driveline"), new Location("Eureka"), new PositionType("Quality control"), new CoreCompetency("Ethical"));
+
+    assertFalse(firstJob.equals(secondJob));
+}
 
 }
